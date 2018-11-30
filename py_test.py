62e85cc58904239e5de57feb8bf510cf6dc738c3
yjwysytaf <yjwysytaf@163.com>
2018-11-30 22:06:50 +0800

    understand how stage works

---
+++ py_test.py
@@ -0,0 +1,21 @@
+print("hello python")
+def get_formatted_name(first_name,last_name):
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+
+    f_name = input("first_name: ")
+    if f_name == 'q' :
+        break
+
+    l_name = input("last_name: ")
+    if l_name == 'q' :
+        break
+
+    formatted_name = get_formatted_name(f_name,l_name)
+    print("\nHello," + formatted_name + "!")
+
+
